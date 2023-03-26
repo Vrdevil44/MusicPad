2a5b1814abb0d2d22a6b775a811caab5f76702df
Vrdevil44 <125772875+Vrdevil44@users.noreply.github.com>
2023-03-26 11:50:36 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,35 +1,92 @@
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
 
-$('body').on("keypress", function (e) {
-    e.preventDefault();
-    e = e || window.event;	
-    console.log('Keypress KeyCode: ' + e.keyCode);
-  });
-  
-  $('body').on("click", '.key-pad', function (e) {
-    e = e || window.event;
-    var id = $(this).attr('id');
-    var keyCode = id.split("-")[1];
-    console.log('Tap KeyCode: ' + keyCode);
-  });
-var BASE_URL = "https://raw.githubusercontent.com/anilrayamajhi/padControllers/master/sounds/sounds";
+var BASE_URL = "https://github.com/Vrdevil44/MusicPad/raw/main/sounds";
+var audioBars = {};
 
 $('body').on("keypress", function(e) {
   e.preventDefault();
   e = e || window.event;
-  var audio = new Audio(`${BASE_URL}/${e.keyCode}.wav`);
-  audio.play();
-  $(`#pad-${e.keyCode}`).fadeOut(20).fadeIn(20);
+  console.log('Keypress KeyCode: ' + e.keyCode);
+
+  if (e.keyCode === 32) {
+    // Stop all audio and remove all bars when spacebar is pressed
+    for (var key in audioBars) {
+      audioBars[key].audio.pause();
+      audioBars[key].audio.currentTime = 0;
+      audioBars[key].bar.removeClass('active');
+      delete audioBars[key];
+    }
+  } else {
+    var audio = new Audio(`${BASE_URL}/${e.keyCode}.wav`);
+    var $bar = $(`#bar-${e.keyCode}`);
+    if (audioBars[e.keyCode]) {
+      // Update the instance of the time bar for the pressed key
+      audioBars[e.keyCode].audio.pause();
+      audioBars[e.keyCode].audio.currentTime = 0;
+      audioBars[e.keyCode].bar.removeClass('active');
+    } else {
+      $bar.addClass('active');
+    }
+    audioBars[e.keyCode] = {audio: audio, bar: $bar};
+    audio.play();
+    $(`#pad-${e.keyCode}`).fadeOut(20).fadeIn(20);
+    audio.addEventListener('timeupdate', function() {
+      var duration = audio.duration;
+      var currentTime = audio.currentTime;
+      var width = (currentTime / duration) * 100;
+      audioBars[e.keyCode].bar.css('width', (100 - width) + '%');
+    });
+  }
 });
 
 $('body').on("click", '.key-pad', function(e) {
   e = e || window.event;
   var id = $(this).attr('id');
   var keyCode = id.split("-")[1];
+  console.log('Tap KeyCode: ' + keyCode);
+
   var audio = new Audio(`${BASE_URL}/${keyCode}.wav`);
+  var $bar = $(`#bar-${keyCode}`);
+  if (audioBars[keyCode]) {
+    // Update the instance of the time bar for the tapped key
+    audioBars[keyCode].audio.pause();
+    audioBars[keyCode].audio.currentTime = 0;
+    audioBars[keyCode].bar.removeClass('active');
+  } else {
+    $bar.addClass('active');
+  }
+  audioBars[keyCode] = {audio: audio, bar: $bar};
   audio.play();
   $(`#pad-${keyCode}`).fadeOut(20).fadeIn(20);
+  audio.addEventListener('timeupdate', function() {
+    var duration = audio.duration;
+    var currentTime = audio.currentTime;
+    var width = (currentTime / duration) * 100;
+    audioBars[keyCode].bar.css('width', (100 - width) + '%');
+  });
 });
 
-particlesJS.load('particles-js', 'particles.json', function() {
-  console.log('callback - particles.js config loaded');
+$('body').on("keyup", function(e) {
+  e = e || window.event;
+  var keyCode = e.keyCode;
+  console.log('Keyup KeyCode: ' + keyCode);
+
+  if (audioBars[keyCode]) {
+    // Remove the time bar when the audio finishes
+    var audioBar = audioBars[keyCode];
+    var audio = audioBar.audio;
+    var bar = audioBar.bar;
+    var duration = audio.duration;
+    var currentTime = audio.currentTime;
+    var remainingTime = duration - currentTime;
+    bar.removeClass('active');
+    delete audioBars[keyCode];
+    if (remainingTime > 0.1) {
+      audio.pause();
+      audio.currentTime = 0;
+    }
+  }
 });
+
